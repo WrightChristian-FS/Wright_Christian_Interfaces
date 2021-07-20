8360c8f030133eceeda05668d07b53e122d7b673
WrightChristian-FS <cmwright@student.fullsail.edu>
2021-07-19 18:56:52 -0700

    Created Menu class and functionality

---
+++ ZooKeeper/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ZooKeeper
+{
+    public class Menu
+    {
+        public class Menu
+        {
+            //Fields
+            private static string _title;
+            private static List<string> _menuItems;
+
+            public static void Init(string title, List<string> menuItems)
+            {
+                _title = title;
+                _menuItems = menuItems;
+            }
+
+            public static void Display()
+            {
+
+                //Header
+                UI.HeaderUI();
+                Console.WriteLine("====================");
+                Console.WriteLine($"   {_title}");
+                Console.WriteLine("====================\r\n");
+                UI.StandardUI();
+
+                //Print the menu options 
+                foreach (string item in _menuItems)
+                {
+                    Console.WriteLine($"{_menuItems.IndexOf(item) + 1}: {item}");
+                }
+            }
+        }
+    }
+}
