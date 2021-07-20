1f6f0eb26ba58b36c03a388fc837ca13ad7ec6d2
WrightChristian-FS <cmwright@student.fullsail.edu>
2021-07-19 19:00:06 -0700

    Created UI Class and Functionality

--- ZooKeeper/UI.cs
+++ ZooKeeper/UI.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 namespace ZooKeeper
 {
+   
     public class UI
     {
         public UI()
         {
-            Console.WriteLine("This is a test");
+        }
+
+        public static void HeaderUI()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+        }
+
+        public static void InputUI()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+        }
+
+        public static void StandardUI()
+        {
+
+            Console.ForegroundColor = ConsoleColor.White;
+
         }
     }
+    
 }
